d6a9fcc23317ed78cdeb367457559bc24f8e53de
Joe Buschmann <joseph.buschmann@outlook.com>
2017-10-27 17:49:11 -0500

    Pickles Documentation

---
+++ pickles.bat
@@ -0,0 +1,8 @@
+.\packages\NUnit.ConsoleRunner.3.6.1\tools\nunit3-console.exe .\bin\Debug\SeleniumAndSpecflow.dll
+
+.\packages\Pickles.CommandLine.2.16.2\tools\pickles.exe^
+	--feature-directory=.\^
+	--output-directory=.\documentation^
+	--link-results-file=.\TestResult.xml^
+	--documentation-format=dhtml^
+	--test-results-format=nunit3
